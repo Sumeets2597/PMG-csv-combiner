f5931a460621cf3bda0a9a6f9cc7be3fb103225f
Sumeet sarode <Sumeet.s2597@gmail.com>
2021-04-03 22:42:44 -0400

    Updating the validity function

--- csv_combiner.py
+++ csv_combiner.py
@@ -10,32 +10,44 @@ import sys
 import pandas as pd
 import os
 
+    
 def csv_combiner(filenames):
+
+    # Function to check validity of all files
+    def _check_for_valid_files(filenames):
     
-    # Check if filenames are given
-    if not filenames: return None
-    
+        # Check if filenames are given
+        if not filenames: return None
+
+        init_cols = None
+        for filename in filenames:
+        
+            # Checking if file exists
+            if not os.path.exists(filename): raise IOError('File Not Found Error: No such file')
+            
+            # Checking for "csv" extension
+            if not filename.endswith('.csv'): raise Exception('Not a valid CSV file.')
+
+            cols = pd.read_csv(filename, nrows=1).columns
+            if not init_cols: init_cols = set(cols)
+
+            # Checking if columns are same as initial file
+            elif init_cols != set(cols): raise Exception('Columns are not same')
+
+        return True
+
+    # Checking for all validity conditions
+    flag = _check_for_valid_files(filenames)
+
     for filename in filenames:
         
-        # Checking if file exists
-        if not os.path.exists(filename): raise IOError('File Not Found Error: No such file')
-        
-        # Checking for "csv" extension
-        if not filename.endswith('.csv'): raise Exception('Not a valid CSV file.')
-    
-    init_cols = None
-    for filename in filenames:
-        
-        cols = list(pd.read_csv(filename, nrows=1).columns) + ['filename']
-        if not init_cols:
+        if flag:
+            cols = list(pd.read_csv(filename, nrows=1).columns) + ['filename']
 
             # Adding columns in the output file
             print(','.join(cols))
 
-            init_cols = set(cols)
-
-        # Checking if columns are same as initial file
-        elif init_cols != set(cols): raise Exception('Columns are not same')
+            flag = False
 
         # Reading the csv file in chunks to avoid memory error
         chunk_size = 64000
